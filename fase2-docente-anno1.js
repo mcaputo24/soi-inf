9944753b436afc18add64fcc0aad7e627fe003ba
mcaputo24 <massi.caputo@gmail.com>
2025-08-06 18:51:26 +0200

    Add files via upload

--- fase2-docente-anno1.js
+++ fase2-docente-anno1.js
@@ -25,7 +25,8 @@ const etichette = {
   data: "Data compilazione",
   agg1: "Aggettivo 1", agg2: "Aggettivo 2", agg3: "Aggettivo 3", agg4: "Aggettivo 4", agg5: "Aggettivo 5",
   agg6: "Aggettivo 6", agg7: "Aggettivo 7", agg8: "Aggettivo 8", agg9: "Aggettivo 9", agg10: "Aggettivo 10",
-  pensiero_lavoro: "Pensiero sul lavoro",
+  pensiero_lavoro: "Un pensiero sul lavoro",
+  cos_e_lavoro: "1) Secondo te, cosa è il lavoro?",
   scheda3: "Scheda 3 – Testo autovalutazione",
   scheda4: "Scheda 4 – Percorsi possibili dopo la scuola",
   'sum-gente': "Scheda 3 – Gente",
@@ -37,7 +38,7 @@ const etichette = {
 const sezioni = {
   'Dati anagrafici': ['cognome', 'nome', 'classe', 'data'],
   'Scheda 1 – Mappa di descrizione di sé': ['agg1','agg2','agg3','agg4','agg5','agg6','agg7','agg8','agg9','agg10'],
-  'Scheda 2 – Un pensiero sul lavoro': ['pensiero_lavoro'],
+  'Scheda 2 – Un pensiero sul lavoro': ['pensiero_lavoro','cos_e_lavoro'],
   'Scheda 3 – Modi di lavorare': ['scheda3','sum-gente','sum-idee','sum-dati','sum-cose'],
   'Scheda 4 – Tutte le possibili strade': ['scheda4']
 };
